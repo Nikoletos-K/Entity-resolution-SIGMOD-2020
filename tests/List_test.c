193fd70e0de2dd3d5a11ae2968ee84003cc09dae
Myrto-Iglezou <61623746+Myrto-Iglezou@users.noreply.github.com>
2020-11-09 18:48:41 +0200

    Update List_test.c

--- tests/List_test.c
+++ tests/List_test.c
@@ -1,18 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
 #include "./../include/acutest.h"			
+#include "./../include/list.h"
 
-#include "./../iclude/list.h"
 
+int compare_ints(void* a, void* b) {
+	return *(int*)a - *(int*)b;
+}
 
 void test_create(void) {
 
 	List *list = createList();
 
-	if(TEST_ASSERT(list != NULL)){
-		TEST_ASSERT(list->firstNode == NULL);
-		TEST_ASSERT(list->lastNode == NULL);
-		TEST_ASSERT(list->numOfNodes == 0);
-	}
-
+	TEST_ASSERT(list != NULL);
+	TEST_ASSERT(list->firstNode == NULL);
+	TEST_ASSERT(list->lastNode == NULL);
+	TEST_ASSERT(list->numOfNodes == 0);
+	
 	deleteList(list);
 }
 
@@ -21,9 +27,8 @@ void test_createNode(void) {
 	int data = 10; 
 	listNode * node = create_listNode((void*) &data);
 
-	if(TEST_ASSERT(node != NULL)){
-		TEST_ASSERT(node->data,&data);
-	}
+	TEST_ASSERT(node != NULL);
+	TEST_ASSERT(compare_ints((void*) node->data , (void*) &data) == 0);
 
 	free(node);
 }
@@ -32,52 +37,22 @@ void test_insert(void) {
 	
 	List *list = createList();
 	
-	int N = 1000;
-	int* array = malloc(N * sizeof(*array));					
+	int N = 10;
+	int* array = malloc(N * sizeof(*array));	
 
 	for (int i = 0; i < N; i++) {
 
-		insert_toList(list,(void *)  &array[i]);
-		
+		insert_toList(list, (void *)  &array[i]);
 		TEST_ASSERT(list->numOfNodes == (i + 1));	
-
-		TEST_ASSERT(list->firstNode->data == &array[i]);	
 	}
 
-	// Ελέγχουμε εάν τα στοιχεία έχουν μπει με την αντίστροφη σειρά
-	ListNode node = list->firstNode;
-
-	for (int i = N - 1; i >= 0; i--) {
-		TEST_ASSERT(node->data == &array[i]);
-		node = node->next;
-	}
-
-	deleteList(list);
-	free(array);
-}
-
-int compare_ints(Pointer a, Pointer b) {
-	return *(int*)a - *(int*)b;
-}
-
-void test_find(void) {
-	List *list = createList();
-	int N = 1000;
-	int* array = malloc(N * sizeof(*array));					
+	listNode* node = list->firstNode;
 
 	for (int i = 0; i < N; i++) {
-		array[i] = i;
-		insert_toList(list,&array[i]);
-	}
 
-	// Εύρεση όλων των στοιχείων
-	for (int i = 0; i < N; i++) {
-		int* value = findValue(list, &i, compare_ints); 
-		TEST_ASSERT(value == &array[i]);
+		TEST_ASSERT(node->data == (void*)  &array[i]);	
+		node = node->nextNode;
 	}
-
-	int not_exists = -1;
-	TEST_ASSERT(findValue(list,(void*) &not_exists, compare_ints) == NULL);
 
 	deleteList(list);
 	free(array);
@@ -87,19 +62,18 @@ void test_one_node_list(void){
 	List *list = createList();
 
 	int data = 10;
-	insert_toList(list,(void*)data);
+	insert_toList(list,(void*) &data);
 	TEST_ASSERT(list->numOfNodes==1);
 	TEST_ASSERT(oneNodeList(list) == 1);
 
 	int data2 = 12;
-	insert_toList(list,(void*)data2);
+	insert_toList(list,(void*) &data2);
 	TEST_ASSERT(oneNodeList(list) != 1);
 }
 
 TEST_LIST = {
 	{ "createList", test_create },
 	{ "insert_toList", test_insert },
-	{ "findValue", test_find },
-	{ "oneNodeList",test_one_node_list}
+	{ "oneNodeList",test_one_node_list},
 	{ NULL, NULL } 
 };
