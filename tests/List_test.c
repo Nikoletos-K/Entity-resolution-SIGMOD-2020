f40789906c2816204fc2da4e9a76320e3691e722
Myrto-Iglezou <61623746+Myrto-Iglezou@users.noreply.github.com>
2020-11-09 13:01:46 +0200

    Update List_test.c
    Finish with list testing

--- tests/List_test.c
+++ tests/List_test.c
@@ -1,4 +1,4 @@
-#include "./../include/acutest.h"			// Απλή βιβλιοθήκη για unit testing
+#include "./../include/acutest.h"			
 
 #include "./../iclude/list.h"
 
@@ -18,33 +18,26 @@ void test_create(void) {
 void test_createNode(void) {
 
 	int data = 10; 
-	listNode * node = create_listNode();
+	listNode * node = create_listNode((void*) &data);
 
-	TEST_ASSERT(list != NULL);
-	TEST_ASSERT(list->firstNode == NULL);
-	TEST_ASSERT(list->lastNode == NULL);
-	TEST_ASSERT(list->numOfNodes == 0);
+	TEST_ASSERT(node != NULL);
 
-	deleteList(list);
+	free(node);
 }
 
 void test_insert(void) {
 	
 	List *list = createList();
-	// Θα προσθέτουμε, μέσω της insert, δείκτες ως προς τα στοιχεία του π΄ίνακα
+	
 	int N = 1000;
 	int* array = malloc(N * sizeof(*array));					
 
 	for (int i = 0; i < N; i++) {
 
-		// list_insert_next(list, LIST_BOF, &array[i]);
-
 		insert_toList(list,(void *)  &array[i]);
 		
-		// Ελέγχουμε εάν ενημερώθηκε (αυξήθηκε) το μέγεθος της λίστας.
 		TEST_ASSERT(list->numOfNodes == (i + 1));	
 
-		// Ελέγχουμε εάν ο πρώτος κόμβος περιέχει σαν τιμή τον δείκτη που μόλις κάναμε insert								
 		TEST_ASSERT(list->firstNode->data == &array[i]);	
 	}
 
@@ -56,27 +49,19 @@ void test_insert(void) {
 		node = node->next;
 	}
 
-	// Εισαγωγή σε ενδιάμεσο κόμβο: προσθέτουμε το NULL σαν δεύτερο κόμβο
-	// ListNode first_node = list->firstNode;
-
-	// list_insert_next(list, first_node, NULL);
-	// TEST_ASSERT(list_node_value(list, list_next(list, first_node)) == NULL);
-
 	deleteList(list);
 	free(array);
 }
 
-// Σύγκριση δύο int pointers
 int compare_ints(Pointer a, Pointer b) {
 	return *(int*)a - *(int*)b;
 }
 
 void test_find() {
-	List list = list_create(NULL);
+	List *list = createList();
 	int N = 1000;
 	int* array = malloc(N * sizeof(*array));					
 
-	// Εισάγουμε δοκιμαστικές τιμές στον πίνακα, για να ελέγξουμε την test_find
 	for (int i = 0; i < N; i++) {
 		array[i] = i;
 		insert_toList(list,&array[i]);
@@ -88,25 +73,30 @@ void test_find() {
 		TEST_ASSERT(value == &array[i]);
 	}
 
-	// Δοκιμάζουμε, για μια τυχαία τιμή που δεν μπορεί πιθανώς να υπάρχει στην λίστα,
-	// αν η list_find γυρνάει σωστά NULL pointer
 	int not_exists = -1;
-	TEST_ASSERT(findValue(list, &not_exists, compare_ints) == NULL);
+	TEST_ASSERT(findValue(list,(void*) &not_exists, compare_ints) == NULL);
 
 	deleteList(list);
 	free(array);
 }
 
-// destroy
-//
-// Η destroy καλείται σε όλα τα tests, για να βρούμε αν δουλεύει σωστά τρέχουμε
-//   make valgrind
+void test_one_node_list(){
+	List *list = createList();
 
+	int data = 10;
+	insert_toList(list,(void*)data);
+	TEST_ASSERT(list->numOfNodes==1);
+	TEST_ASSERT(oneNodeList(list) == 1);
 
-// Λίστα με όλα τα tests προς εκτέλεση
+	int data2 = 12;
+	insert_toList(list,(void*)data2);
+	TEST_ASSERT(oneNodeList(list) != 1);
+}
+
 TEST_LIST = {
 	{ "createList", test_create },
 	{ "insert_toList", test_insert },
 	{ "findValue", test_find },
-	{ NULL, NULL } // τερματίζουμε τη λίστα με NULL
+	{ "oneNodeList",test_one_node_list}
+	{ NULL, NULL } 
 };
