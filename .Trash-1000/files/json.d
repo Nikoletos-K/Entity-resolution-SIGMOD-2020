84cdfba7740cab381890dd6173626d064561426b
Nikoletos Konstantinos <47646955+Nikoletos-K@users.noreply.github.com>
2020-12-03 19:35:47 +0200

    removed-all

--- .Trash-1000/files/json.d
+++
@@ -1,2 +0,0 @@
-json.o: json.c ../include/json.h ../include/./HashTable.h \
- ../include/././RBTree.h ../include/./list.h ../include/./DisJointSet.h
--- .Trash-1000/files/utils.d
+++
@@ -1,3 +0,0 @@
-utils.o: utils.c ../include/utils.h ../include/./json.h \
- ../include/././HashTable.h ../include/./././RBTree.h \
- ../include/././list.h ../include/././DisJointSet.h
