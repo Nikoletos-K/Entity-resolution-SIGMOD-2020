79b14457b452712359a288405cee3ac7ec769e73
Myrto-Iglezou <61623746+Myrto-Iglezou@users.noreply.github.com>
2020-12-11 22:08:55 +0200

    Update LogisticRegression.h
    y is integer, not float

--- include/LogisticRegression.h
+++ include/LogisticRegression.h
@@ -17,10 +17,11 @@ typedef struct LogisticRegression {
 
 
 LogisticRegression* LR_construct(size_t vectorSize);
-void LR_train(LogisticRegression* model,float * x_vector,float y,float learning_rate,float threshold);
-float LR_predict(LogisticRegression* model,float * x_vector);
+void LR_train(LogisticRegression* model,float * x_vector,int y,float learning_rate,float threshold);
+int LR_predict(LogisticRegression* model,float * x_vector);
 void LR_destroy(LogisticRegression* model);
 float CrossEntropy(float prediction,float * x_vector,float y_vector, size_t vectorSize);
 
 int decision_boundary(float probability);
 float sigmoid(float x);
+float accuracy(int * prediction_labels,int * true_labels,int numOfLabels);
